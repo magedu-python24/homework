b5eea0cc2645ec3c7c4a34e5de57bcfdcaa60f72
lucascheng <chengliang6@outlook.com>
2019-07-30 15:50:14 +0800

    homework for the second week.

---
+++ P24013-程亮/the_second_week/calculator.py
@@ -0,0 +1,17 @@
+#！/bin/env python3
+
+while True:
+	a=input('please input a number: ')
+	if not a:
+		break
+	opt=str(input('please input an operator, such as * / + -, keep space will exit: '))
+	b=input('please input another number: ')
+	if opt=='*':
+		print('result is',int(a)*int(b))
+	elif opt=='/':
+		print('result is',int(a)/int(b))
+	elif opt=='+':
+		print('result is',int(a)+int(b))
+	else:
+		print('result is',int(a)-int(b))
+	del a
