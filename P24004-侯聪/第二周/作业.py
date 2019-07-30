834ee559cb21f1b3c5aeb2789557c005abdd4a0a
houcong <conghou24@163.com>
2019-07-30 15:08:04 +0800

    添加第二周作业

---
+++ P24004-侯聪/第二周/作业.py
@@ -0,0 +1,17 @@
+while True:
+    x = int(input("Input the First Number>>>"))
+    cs = input ("Input the Calculating signs + = * />>>")
+    list = ['+','-','*','/']
+    if cs not in list:
+        print("The Calculation signs U input is not Correct!!!!")
+        continue
+    y = int(input("Input the Second Number>>>"))
+
+    if cs == "+":
+        print(x,"+",y,+"=",x+y)
+    elif cs == "-":
+        print(x,"-",y,"=",x-y)
+    elif cs == "*":
+        print(x,"*",y,"=",x*y)
+    else :
+        print(x,"/",y,"=",x/y)
