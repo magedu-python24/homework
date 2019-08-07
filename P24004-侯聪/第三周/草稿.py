f3a4fb6045fbdef3d9e7005b4c59ee0888333f3d
houcong <conghou24@163.com>
2019-08-07 16:06:40 +0800

    添加第三周作业

---
+++ P24004-侯聪/第三周/草稿.py
@@ -0,0 +1,13 @@
+str = "123"
+str1.capitalize(   str1.isalpha(      str1.ljust(        str1.split(
+str1.casefold(     str1.isascii(      str1.lower(        str1.splitlines(
+str1.center(       str1.isdecimal(    str1.lstrip(       str1.startswith(
+str1.count(        str1.isdigit(      str1.maketrans(    str1.strip(
+str1.encode(       str1.isidentifier( str1.partition(    str1.swapcase(
+str1.endswith(     str1.islower(      str1.replace(      str1.title(
+str1.expandtabs(   str1.isnumeric(    str1.rfind(        str1.translate(
+str1.find(         str1.isprintable(  str1.rindex(       str1.upper(
+str1.format(       str1.isspace(      str1.rjust(        str1.zfill(
+str1.format_map(   str1.istitle(      str1.rpartition(
+str1.index(        str1.isupper(      str1.rsplit(
+str1.isalnum(      str1.join(         str1.rstrip(
